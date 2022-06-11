f118dca9ee3cec902a77a1d7ea291ea91d19b254
pedram-tehranchi <96863912+pedram-tehranchi@users.noreply.github.com>
2022-06-11 22:08:46 +0430

    Update 3-📝_Outlier_Report.py

--- pages/3-📝_Outlier_Report.py
+++ pages/3-📝_Outlier_Report.py
@@ -10,7 +10,7 @@ st.sidebar.markdown("# Outlier Report 📝")
 st.markdown("")
 
 cwd = os.getcwd()
-df = pd.read_csv(f'{cwd}\data.csv')
+df = pd.read_csv(f'{cwd}data.csv')
 
 st.markdown("<h4 style='text-align: center;'> Raw data</h4>", unsafe_allow_html=True)
 fig_1 = px.line(
