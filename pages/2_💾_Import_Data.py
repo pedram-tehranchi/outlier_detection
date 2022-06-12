bd2b27ac48275cfe2088b7fc9e3dd48b84eb1899
pedram-tehranchi <96863912+pedram-tehranchi@users.noreply.github.com>
2022-06-12 11:36:13 +0430

    Update 2_💾_Import_Data.py

--- pages/2_💾_Import_Data.py
+++ pages/2_💾_Import_Data.py
@@ -7,6 +7,6 @@ uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=Fal
 if uploaded_files is not None:
     df = pd.read_csv(uploaded_files)
     st.dataframe(df.head(15))
-    df.to_csv("data.csv", index=False)
+    df.to_csv("pages/data.csv", index=False)
 
 st.sidebar.markdown("# import data 💾")
