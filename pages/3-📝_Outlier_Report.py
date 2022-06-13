b3bb8a9cf3a85f02fc1a435c71a6c06c4be5b6d1
pedram-tehranchi <96863912+pedram-tehranchi@users.noreply.github.com>
2022-06-13 19:00:02 +0430

    Update 3-📝_Outlier_Report.py

--- pages/3-📝_Outlier_Report.py
+++ pages/3-📝_Outlier_Report.py
@@ -23,18 +23,15 @@ fig_1 = px.line(
 st.plotly_chart(fig_1, use_container_width=True)
 
 df['actual_facility'] = df['facility']
-df['zscore'] = (df.facility - df.facility.mean())/df.facility.std(ddof=0)
-outliers = df[df['zscore']>=3]
-
-df['facility'] = np.where(df['zscore']>=3, None, df['facility'])
-df['zscore'] = np.where(df['zscore']>=3, None, df['zscore'])
+df['zscore_2'] = (df.facility - df.facility.mean())/df.facility.std(ddof=0)
+df['normalized_facility'] = np.where(abs(df['zscore_2'])>=3, None, df['facility'])
 df = df.ffill()
 
 st.markdown("<h4 style='text-align: center;'> step 1</h4>", unsafe_allow_html=True)
 fig_2 = px.line(
         df,
         x="time",
-        y=["facility"]
+        y=["normalized_facility"]
         #,color="Real Data"
         # barmode="group",
     )
@@ -45,24 +42,23 @@ st.plotly_chart(fig_2, use_container_width=True)
 # df['facility'] = np.where(df['diff']>=2, df['mean_50'], df['facility'])
 
 
-df['median_20'] = df['facility'].rolling(51, center=True).median()
-df['std_20'] = df['facility'].rolling(51, center=True).std()
-df['zscore'] = (df['facility'] - df['median_20'])/df['std_20']
-df['facility'] = np.where(abs(df['zscore'])>=4, None, df['facility'])
+df['median_20'] = df['normalized_facility'].rolling(51, center=True).median()
+df['std_20'] = df['normalized_facility'].rolling(51, center=True).std()
+df['zscore'] = (df['normalized_facility'] - df['median_20'])/df['std_20']
+df['normalized_facility'] = np.where(abs(df['zscore'])>=4, None, df['normalized_facility'])
 df = df.ffill()
 
 st.markdown("<h4 style='text-align: center;'> step 2</h4>", unsafe_allow_html=True)
 fig_3 = px.line(
         df,
         x="time",
-        y=["facility"]
+        y=["normalized_facility"]
         #,color="Real Data"
         # barmode="group",
     )
 st.plotly_chart(fig_3, use_container_width=True)
 
 
-df['normalized_facility'] = df['facility']
 st.markdown("<h4 style='text-align: center;'> Actual Facility VS Normalized Facility</h4>", unsafe_allow_html=True)
 fig_4 = px.line(
         df,
@@ -75,18 +71,16 @@ st.plotly_chart(fig_4, use_container_width=True)
 
 
 st.markdown("<h4 style='text-align: center;'> outlier code:</h4>", unsafe_allow_html=True)
-code = '''def outlier_normalizer(raw_data):
-    raw_data['actual_facility'] = raw_data['facility']
-    raw_data['zscore'] = (raw_data.facility - raw_data.facility.mean())/raw_data.facility.std(ddof=0)
-    raw_data['facility'] = np.where(raw_data['zscore']>=3, None, raw_data['facility'])
-    raw_data['zscore'] = np.where(raw_data['zscore']>=3, None, raw_data['zscore'])
-    raw_data = raw_data.ffill()
-    raw_data['median_20'] = raw_data['facility'].rolling(51, center=True).median()
-    raw_data['std_20'] = raw_data['facility'].rolling(51, center=True).std()
-    raw_data['zscore'] = (raw_data['facility'] - raw_data['median_20'])/raw_data['std_20']
-    raw_data['facility'] = np.where(abs(raw_data['zscore'])>=4, None, raw_data['facility'])
-    raw_data = raw_data.ffill()
-    raw_data['normalized_facility'] = raw_data['facility']
+code = '''def outlier_normalizer(df):
+    df['actual_facility'] = df['facility']
+    df['zscore_2'] = (df.facility - df.facility.mean())/df.facility.std(ddof=0)
+    df['normalized_facility'] = np.where(abs(df['zscore_2'])>=3, None, df['facility'])
+    df = df.ffill()
+    df['median_20'] = df['normalized_facility'].rolling(51, center=True).median()
+    df['std_20'] = df['normalized_facility'].rolling(51, center=True).std()
+    df['zscore'] = (df['normalized_facility'] - df['median_20'])/df['std_20']
+    df['normalized_facility'] = np.where(abs(df['zscore'])>=4, None, df['normalized_facility'])
+    df = df.ffill()
     raw_data.drop(columns=['facility'], inplace=True)
-    return raw_data'''
+    return df'''
 st.code(code, language='python')
